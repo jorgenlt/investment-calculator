9a01d78d8c8d038c4c67d01b25e2626e6dce997f
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2024-09-12 14:31:56 +0200

    stylesheet

--- app/(tabs)/(home)/_layout.tsx
+++ app/(tabs)/(home)/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from "expo-router";
 export default function HomeLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerTitle: "Home" }} />
+      <Stack.Screen name="index" options={{ headerTitle: "Investment Calculator" }} />
     </Stack>
   );
 }
--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -1,17 +1,30 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 import {Link} from 'expo-router'
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
-      <Text>Edit app/(tabs)/(home)/index.tsx to edit this screen.</Text>
-      <Link href="/settings">Go to Settings</Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    // flex: 1,
+    // justifyConent: "center",
+    // alignItems: "center",
+  }
+})
+
+{/* <View
+style={{
+  flex: 1,
+  justifyContent: "center",
+  alignItems: "center",
+}}
+>
+<Text>Edit app/(tabs)/(home)/index.tsx to edit this screen.</Text>
+<Link href="/settings">Go to Settings</Link>
+</View> */}
