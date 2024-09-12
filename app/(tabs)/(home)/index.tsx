2300e039c4f08da7f788442297067431b8b8b962
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2024-09-12 15:01:50 +0200

    startig with input forms

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -1,11 +1,44 @@
-import { Text, View, StyleSheet } from "react-native";
-import {Link} from 'expo-router'
+import { Text, View, StyleSheet, TextInput } from "react-native";
+import { useState } from "react";
+import { Link } from "expo-router";
 
 export default function Index() {
+  const [initialValue, setInitialValue] = useState("");
+  const [interestRate, setInterestRate] = useState("");
+  const [duration, setDuration] = useState("");
+  const [monthlyDeposit, setMonthlyDeposit] = useState("");
+
+
   return (
-    <View
-      style={styles.container}
-    >
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        onChangeText={setInitialValue}
+        value={initialValue}
+        placeholder="Initial value"
+        keyboardType="numeric"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={setInterestRate}
+        value={interestRate}
+        placeholder="Interest rate"
+        keyboardType="numeric"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={setDuration}
+        value={duration}
+        placeholder="Duration (years)"
+        keyboardType="numeric"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={setMonthlyDeposit}
+        value={monthlyDeposit}
+        placeholder="Montly deposit"
+        keyboardType="numeric"
+      />
     </View>
   );
 }
@@ -15,10 +48,17 @@ const styles = StyleSheet.create({
     // flex: 1,
     // justifyConent: "center",
     // alignItems: "center",
-  }
-})
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
 
-{/* <View
+{
+  /* <View
 style={{
   flex: 1,
   justifyContent: "center",
@@ -27,4 +67,5 @@ style={{
 >
 <Text>Edit app/(tabs)/(home)/index.tsx to edit this screen.</Text>
 <Link href="/settings">Go to Settings</Link>
-</View> */}
+</View> */
+}
